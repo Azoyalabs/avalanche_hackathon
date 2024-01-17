44a6745086eac81d46e866559ca0ddbc8be70d7b
Wally869 <wally869@protonmail.com>
2024-01-17 13:42:30 +0200

    added minting scripts

--- contracts/scripts/mint_reader_token.ts
+++ contracts/scripts/mint_reader_token.ts
@@ -14,35 +14,54 @@ import { CONTRACT_ADDRESS } from "./constants";
 // console.log(process.env.ADMIN_PASSPHRASE)
 
 async function main() {
-    const account = mnemonicToAccount(process.env.ADMIN_PASSPHRASE!!)
-    console.log(account.address)
+    const admin_account = mnemonicToAccount(process.env.ADMIN_PASSPHRASE!!, {
+        //changeIndex: 1
+    })
+
+    const user_account = mnemonicToAccount(process.env.USER_PASSPHRASE!!, {
+        //changeIndex: 1
+    })
+    
+    //console.log(account.address)
+
+    const wallet = createWalletClient(
+        {
+            account: user_account,
+            chain: avalancheFuji,
+            transport: http(avalancheFuji.rpcUrls.public.http[0])
+        }
+    )
+
+    //console.log(await wallet.getAddresses())
 
     const publicClient = createPublicClient({
         chain: avalancheFuji,
         transport: http(avalancheFuji.rpcUrls.public.http[0])
     })
 
-    const wallet = createWalletClient(
-        {
-            account: account,
-            chain: avalancheFuji,
-            transport: http(avalancheFuji.rpcUrls.public.http[0])
-        }
-    )
 
     let tokenId = await publicClient.readContract({
         address: CONTRACT_ADDRESS,
         abi: contractData.abi,
         functionName: "createTokenId",
-        args: [account.address, BigInt(0), false],  
+        args: [admin_account.address, BigInt(0), false],  // creator address, article id, is_paying
     });
+
+    // get mint price if it's a paid article 
+    let _mint_price = await publicClient.readContract({
+        address: CONTRACT_ADDRESS,
+        abi: contractData.abi,
+        functionName: "mintPrice",
+        args: []
+    }) 
+
 
     const res = await wallet.writeContract({
         address: CONTRACT_ADDRESS,
         abi: contractData.abi,
         functionName: "mint",
-        args: [account.address, tokenId, BigInt(1), "0x"],
-        account
+        args: [user_account.address, tokenId, BigInt(1), "0x"],
+        account: user_account
     })
 
 
--- contracts/scripts/network.ts
+++
@@ -1,8 +0,0 @@
-
-
-export const AVALANCHE_TESTNET = {
-    rpc: "https://api.avax-test.network/ext/bc/C/rpc",
-    chainId: 43113,
-    symbol: "AVAX",
-    explorer: "https://testnet.snowtrace.io"
-}
