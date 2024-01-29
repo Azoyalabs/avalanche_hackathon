b0695aa2ee157e382651c9d651298b3afc6181d1
Wally869 <wally869@protonmail.com>
2024-01-29 13:33:26 +0200

    adding happy path tests for minting through transferAndCall, both writer and reader

--- contracts/test/Summit.ts
+++ contracts/test/Summit.ts
@@ -76,7 +76,6 @@ describe("Summit", function () {
         it("Id should be matching", async function () {
             const { bnmToken, summit, summitReceiver, articleWriter, contractOwner, userLambda } = await loadFixture(deployContracts);
 
-
             [articleWriter.account.address, contractOwner.account.address, userLambda.account.address].forEach((addr) => {
                 [BigInt(0), BigInt(10), BigInt(4684)].forEach((articleId) => {
                     [false, true].forEach(async (isPaying) => {
@@ -126,9 +125,7 @@ describe("Summit", function () {
                     account: userLambda.account
                 }
             )).to.be.rejectedWith("Unauthorized");
-
         })
-
     })
 
     describe("Minting", function () {
@@ -149,16 +146,128 @@ describe("Summit", function () {
             })
         })
 
-        /*
-        describe("TransferAndCall - ERC677-based workflow", function() {
-            it("Writer can create a new free article", async function () {
-                throw "Not implemented - Need to check for general message passing in this case"
 
+        describe("TransferAndCall - ERC677-based workflow", function () {
+            it("Writer can create a new free article", async function () {
+                const { bnmToken, summit, summitReceiver, articleWriter, articleReader, contractOwner } = await loadFixture(deployContracts);
+
+                // create tokenId for minting 
+                const tokenId = await summit.read.createTokenId(
+                    [articleWriter.account.address, BigInt(0), false]
+                );
+
+                // create the article 
+                await bnmToken.write.transferAndCall(
+                    [
+                        summitReceiver.address,
+                        BigInt(0),
+                        bytesToHex(toBytes(tokenId))
+                    ],
+                    {
+                        account: articleWriter.account
+                    }
+                );
+            })
+
+            it("Writer can create a new paying article", async function () {
+                const { bnmToken, summit, summitReceiver, articleWriter, articleReader, contractOwner } = await loadFixture(deployContracts);
+
+                // create tokenId for minting 
+                const tokenId = await summit.read.createTokenId(
+                    [articleWriter.account.address, BigInt(0), true]
+                );
+
+                // create the article 
+                await bnmToken.write.transferAndCall(
+                    [
+                        summitReceiver.address,
+                        BigInt(0),
+                        bytesToHex(toBytes(tokenId))
+                    ],
+                    {
+                        account: articleWriter.account
+                    }
+                );
+            })
+
+
+            it("Reader can successfully mint existing free article", async function () {
+                const { bnmToken, summit, summitReceiver, articleWriter, articleReader, contractOwner } = await loadFixture(deployContracts);
+
+                // set role for minting 
+                await bnmToken.write.grantMintRole([contractOwner.account.address]);
+                // mint tokens 
+                await bnmToken.write.mint([articleReader.account.address, BigInt(500)]);
+
+                // create tokenId for minting 
+                const tokenId = await summit.read.createTokenId(
+                    [articleWriter.account.address, BigInt(0), false]
+                );
+
+                // create the article 
+                await bnmToken.write.transferAndCall(
+                    [
+                        summitReceiver.address,
+                        BigInt(0),
+                        bytesToHex(toBytes(tokenId))
+                    ],
+                    {
+                        account: articleWriter.account
+                    }
+                );
+
+                // mint the token for the reader 
+                await expect(bnmToken.write.transferAndCall(
+                    [
+                        summitReceiver.address,
+                        BigInt(100),
+                        bytesToHex(toBytes(tokenId))
+                    ],
+                    {
+                        account: articleReader.account
+                    }
+                )); //.to.be.rejectedWith("Unauthorized");
+            })
+
+            it("Reader can successfully mint existing paying article", async function () {
+                const { bnmToken, summit, summitReceiver, articleWriter, articleReader, contractOwner } = await loadFixture(deployContracts);
+
+                // set role for minting 
+                await bnmToken.write.grantMintRole([contractOwner.account.address]);
+                // mint tokens 
+                await bnmToken.write.mint([articleReader.account.address, BigInt(500)]);
+
+                const articlePrice = await summit.read.mintPrice();
+
+                // direct payment endpoint will call target token, so here we try with the transferAndCall 
+                // create tokenId for minting 
+                const tokenId = await summit.read.createTokenId(
+                    [articleWriter.account.address, BigInt(0), true]
+                );
+
+                // create the article 
+                await bnmToken.write.transferAndCall(
+                    [
+                        summitReceiver.address,
+                        BigInt(0),
+                        bytesToHex(toBytes(tokenId))
+                    ],
+                    {
+                        account: articleWriter.account
+                    }
+                );
+
+                await expect(bnmToken.write.transferAndCall(
+                    [
+                        summitReceiver.address,
+                        articlePrice,
+                        bytesToHex(toBytes(tokenId))
+                    ],
+                    {
+                        account: articleReader.account
+                    }
+                )); //.to.be.rejectedWith("Unauthorized");
             })
         })
-        */
-
-
-
     })
 })
