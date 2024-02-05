b7c4b76cf82187c959a95b3a0c594343f8a8a01d
Maxime Bele <maxime.bele@gmail.com>
2024-02-05 22:40:09 +0100

    nft renderer

--- apps/web/src/routes/api/nfts/[id]/render/+server.ts
+++ apps/web/src/routes/api/nfts/[id]/render/+server.ts
@@ -1,10 +1,46 @@
-import { json } from '@sveltejs/kit';
+import satori from 'satori';
+import { Resvg } from '@resvg/resvg-js';
+import NotoSans from '$lib/assets/NotoSans-Regular.ttf';
+import { read } from '$app/server';
+import { html as toReactNode } from 'satori-html';
+import NftRender from './NftRender.svelte';
+
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async ({ params, url }) => {
-	
+const fontData = read(NotoSans).arrayBuffer();
 
-	return new Response("I'm rendering");
+const height = 630;
+const width = 1200;
+
+export const GET: RequestHandler = async ({ params, url }) => {
+	const result = NftRender.render();
+	const element = toReactNode(`${result.html}<style>${result.css.code}</style>`);
+	const svg = await satori(element, {
+		fonts: [
+			{
+				name: 'Noto Sans',
+				data: await fontData,
+				style: 'normal'
+			}
+		],
+		height,
+		width
+	});
+
+	const resvg = new Resvg(svg, {
+		fitTo: {
+			mode: 'width',
+			value: width
+		}
+	});
+
+	const image = resvg.render();
+
+	return new Response(image.asPng(), {
+		headers: {
+			'content-type': 'image/png'
+		}
+	});
 };
 
 export type yep = Awaited<ReturnType<typeof GET>>;
---
+++ apps/web/src/routes/api/nfts/[id]/render/NftRender.svelte
@@ -0,0 +1,38 @@
+<script>
+	import '../../../../../app.pcss';
+	let src =
+		'https://mirror.xyz/_next/image?url=https%3A%2F%2Fimages.mirror-media.xyz%2Fpublication-images%2FVSOhY7aTvRRVzBALIeyjV.jpeg%3Fheight%3D540%26width%3D1080&w=640&q=75';
+	// 			src="https://mirror.xyz/_next/image?url=https%3A%2F%2Fimages.mirror-media.xyz%2Fpublication-images%2FVSOhY7aTvRRVzBALIeyjV.jpeg%3Fheight%3D540%26width%3D1080&w=640&q=75"
+
+	let width;
+	let height;
+</script>
+
+<div class="flex h-full bg-black dark">
+	<div class="flex w-full h-full" bind:clientWidth={width} bind:clientHeight={height}>
+		<div class="flex flex-col items-center justify-center w-full h-full">
+			<img
+				src="https://images.mirror-media.xyz/publication-images/VSOhY7aTvRRVzBALIeyjV.jpeg"
+				class="object-cover w-full h-full"
+			/>
+			<!-- src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png" -->
+			<!-- 
+			<div
+				class="flex w-full bg-center bg-cover aspect-video"
+				style="background: url('https://mirror.xyz/_next/image?url=https%3A%2F%2Fimages.mirror-media.xyz%2Fpublication-images%2FVSOhY7aTvRRVzBALIeyjV.jpeg%3Fheight%3D540%26width%3D1080&w=640&q=75');"
+			></div>
+			 -->
+		</div>
+
+		<div class="absolute left-[33.33%] z-10 flex h-full w-2/3 flex-col bg-black text-white">
+			<div class="flex flex-col p-4">
+				<h1 class="text-3xl font-bold leading-tight text-pretty">
+					hello worldhello worldhello worldhello worldhello worldhello worldhello world
+				</h1>
+				<div class="flex">
+					<div class="flex">Author Name</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
