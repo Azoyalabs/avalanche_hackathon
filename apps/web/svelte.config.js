7fd93d4365539e7ca734f0d566444174d6da1cd5
Maxime Bele <maxime.bele@gmail.com>
2024-02-07 15:08:52 +0100

    add VITE_MODE env variable to differenciate between vercel and local which requires node

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -3,27 +3,22 @@ import adapterNode from '@sveltejs/adapter-node';
 import adapterVercel from '@sveltejs/adapter-vercel';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { defineConfig } from 'vite';
 
-const config = defineConfig(({ mode }) => {
-	/** @type {import('@sveltejs/kit').Config} */
-	const config = {
-		// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-		// for more information about preprocessors
-		preprocess: [vitePreprocess({})],
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [vitePreprocess({})],
 
-		kit: {
-			// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-			// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-			// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-			adapter: mode === 'production' ? adapterVercel() : adapterNode(),
-			env: {
-				dir: 'env'
-			}
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: process.env.VITE_MODE === 'vercel' ? adapterVercel() : adapterNode(),
+		env: {
+			dir: 'env'
 		}
-	};
-	return config;
-})();
+	}
+};
 
-//export default config;
 export default config;
